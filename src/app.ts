66a3a00fea27c35f33bc1e95423256a323109396
RTQ <tema34468@gmail.com>
2025-07-27 18:09:52 +0300

    Init

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+
+const PORT = process.env.PORT || 1207;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
