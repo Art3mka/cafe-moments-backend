83c825292be269019bea8d9a7ddbd08aeb9657a2
RTQ <tema34468@gmail.com>
2025-07-31 13:49:53 +0300

    add admin feature to users

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,12 +1,16 @@
 import { Router } from "express";
 import {
+  changeUserRole,
   createAddress,
   deleteAddress,
   getAllAddresses,
+  getAllUsers,
+  getUserById,
   updateUser,
 } from "../controllers/usersController";
 import { errorHandler } from "../errorHandler";
 import { isAuth } from "../middleware/isAuth";
+import { isAdmin } from "../middleware/isAdmin";
 
 export const userRouter: Router = Router();
 
@@ -17,3 +21,7 @@ userRouter.delete("address/:id", [isAuth], errorHandler(deleteAddress));
 userRouter.get("address/", [isAuth], errorHandler(getAllAddresses));
 
 userRouter.put("/", [isAuth], errorHandler(updateUser));
+
+userRouter.put("/:id/role", [isAuth, isAdmin], errorHandler(changeUserRole));
+userRouter.get("/", [isAuth, isAdmin], errorHandler(getAllUsers));
+userRouter.get("/:id", [isAuth, isAdmin], errorHandler(getUserById));
